d9a58932e3a71e404464946a7cfec7d96c7dcf4c
Juan Jiménez <juan.jimenez@fis.ucm.es>
2024-05-20 13:57:23 +0200

    getting on multi-unicycle algorithm

--- consensus_algorithm/ghf_multi_unicicle.py
+++ consensus_algorithm/ghf_multi_unicicle.py
@@ -23,7 +23,7 @@ def fun(w):
     f1 = 10*np.sin(w) + 20*np.sin(2*w)
     f2 = 10*np.cos(w)-20*np.cos(2*w)
     f3 = -20*np.sin(3*w)
-    return np.array([f1, f2, f3])
+    return np.array([[f1], [f2], [f3]])
 
 def dfun(w):
     '''
@@ -32,7 +32,7 @@ def dfun(w):
     df1 = 10*np.cos(w) + 40*np.cos(2*w)
     df2 = -10*np.sin(w) + 40*np.sin(2*w)
     df3 = -60*np.cos(3*w)
-    return np.array([df1, df2, df3])
+    return np.array([[df1], [df2], [df3]])
     
 def ddfun(w):
     '''
@@ -41,7 +41,7 @@ def ddfun(w):
     ddf1 = -10*np.sin(w) - 40*np.sin(2*w)
     ddf2 = -10*np.cos(w) + 80*np.cos(2*w)
     ddf3 = 180*np.sin(3*w)
-    return np.array([ddf1, ddf2, ddf3])
+    return np.array([[ddf1], [ddf2], [ddf3]])
 
 #matriz lalpaciana de un grafo conexion en daisy chain
 def grafo_L(N):
@@ -59,32 +59,68 @@ def grafo_L(N):
     L[-1, 0] = -1
     return L
 
-def din_agen(i,xi,K,kc,C,v):
+
+def din_agen(N,x,K,kc,Kth,L,v,E,wstr):
     ''' 
     Esta funcion define la dinamica de un agente
     incluye la guia del campo y el efecto de la  cordinación
     Asumo que la dimensión de movimiento en R^3, por tanto (-1)^n = (-1)^3 = -1
     '''
-    #calculamos el valor del campo guía en la posición del agente
-    w = xi[3]
-    p = xi[0:3]
-    f = fun(w)
-    df = dfun(w)
-    ddf = ddfun(w)
-    Kphi = K@(p-f)
-    Kdf  = K@df
     
-    xi = -df-Kphi
-    xi = np.append(xi,-1+Kphi@df + kc*C)
-    nxip = np.linalg.norm(xi[0:2])
-    bxip = xi[0:2]/nxip
-    Jxip = np.append(-K,Kdf,axis=0)
-    #definimos la dinámica y la ley de control
-    dp1 = v*np.cos(p[5])
-    dp2 = v*np.sin(p[5])
-    dp3 = v*xi[2]/nxip
-    dp4 = v*xi[4]/nxip
+    c = -L@(x[:,4] - wstr)
+    
+    for i in range(N):
+        #calculamos el valor del campo guía en la posición del agente
+        xi =x[i:i+1,:].T
+        w = xi[3,0]
+        p = xi[0:3]
+        f = fun(w)
+        df = dfun(w)
+        ddf = ddfun(w)
+        Kphi = K@(p-f)
+        Kdf  = K@df
+        Xi = np.vstack((-df-Kphi,-1+Kphi.T@df + kc*c[i]))
+        nXi = np.linalg.norm(Xi)
+        Xib = Xi/nXi
+        nXip = np.linalg.norm(Xi[0:2])
+        bXip = xi[0:2]/nXip
+        Jpf =np.hstack( \
+            (np.vstack((-K,Kdf.T)),\
+             np.zeros((4,i)),\
+             np.vstack((-ddf + K@df,Kphi.T@ddf-np.ones((1,3))@K@df**2)),np.zeros((4,N-1-i))))
+        Jcr = np.hstack((np.zeros((4,3)),np.vstack((np.zeros((3,N)),-L[i,:]))))
+        Jxip = (np.eye(4)-Xib@Xib.T)@(Jpf+kc*Jcr)/
     
     
     
     
+        #definimos la dinámica y la ley de control
+        dp1 = v*np.cos(xi[5]) #x
+        dp2 = v*np.sin(xi[5]) #y
+        dp3 = v*xi[2]/nXip    #z
+        dp4 = v*xi[4]/nXip    #w (la coordenada virtual)
+        dp5 = -(Xip/nXip).T@E@Xipd/nXi #theta       
+    
+    
+###########################Integracion del modelo#############################
+
+
+#Parámetro de simulación 
+#dimensiones del espacio 3D
+N = 20           #nº de agentes 
+K =np.diag([1,1,2]) #ganancias
+kc = 200 
+L = grafo_L(N) #laplaciana daisy chain de la formacion
+v = 2 #velocidad fija de los uniciclos
+#coordenadas iniciales de los robots 
+#pos = np.random.rand(N, n)*100 #filas: dimensiones
+                           #columnas: nº de robots
+p = np.random.randint(-50, 51, size=(N, 3))
+theta = 2*np.pi*np.random.random((N,1))
+
+#añadimos a la matriz de posiciones la coordenada virtual w 
+w = np.ones((N,1)) #ejemplo: todos valen 1
+wstr = np.arange(0,2*np.pi,2*np.pi/N)    #distribución deseada
+x = np.hstack((p,w,theta))
+
+sol = sl(vector_field, (0,200),Xi[0],method='LSODA',args=(ki,n,N,ww,kc,L),max_step=0.2) 
