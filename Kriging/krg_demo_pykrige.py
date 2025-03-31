5741b9bdabef83cc484c014d3b767c92e2e7108d
Juan Jiménez <juan.jimenez@fis.ucm.es>
2025-03-31 12:02:34 +0200

    los dos modelos de krigin listos

--- Kriging/krg_demo_pykrige.py
+++ Kriging/krg_demo_pykrige.py
@@ -2,12 +2,14 @@
 """
 Created on Fri Mar 28 16:44:04 2025
 Playing with kriging and other fancy stuff
-Calculado con los parametros del artículo para ver qué sale
+but this time using the module pykrige instead of gstools to calculate
+the kriging model
 @author: abierto
 """
 import numpy as np
 from matplotlib import pyplot as plt
 import gstools as gs
+import pykrige as pkr
 plt.close('all')
 #esta función me permite crear campos con máximos donde me de la
 #gana y perfil gausiano.
@@ -23,7 +25,7 @@ def gausianilla(x,Sig,mu,nor=1):
         DESCRIPTION.
     Sig : TYPE Matriz definida positiva, nos permite girar y estirar
     o encoger la distribución
-        DESCRIPTION.plt.title('campo V')
+        DESCRIPTION.
     mu : TYPE numpy array de shape [n,] ó [n,1]
         DESCRIPTION. centro de la distribución
 
@@ -72,16 +74,20 @@ for i in range(mogo.shape[0]):
         V[i,j] = 100*gausianilla(mogo[i,j,:],sigmar,mu,norm) + \
         200*gausianilla(mogo[i,j,:],sigmar1,mu1,norm1) +\
         10*gausianilla(mogo[i,j,:],sigmar2,mu2,norm2)
+
+#pinto en 3D mi campo
 ax = plt.figure().add_subplot(projection='3d')
 ax.plot_wireframe(xm,ym,V)
 plt.xlabel('x')
 
 #ahora viene lo mas importante de toda la fiesta, que es definirse
 #un modelo de covarianza. En realidad, parece que define un modelo
-# completo, en el que se considera, la covarianza, la correlación y 
+# completo, en el que se cosidera, la covarianza, la correlación y 
 #el semivariograma. La clave del modelo, esta, en el como se defina
-#la correlación.
-modelito = gs.Gaussian(dim=2,var=0.5,len_scale=50)
+#la correlación. Ojo a la diferencia de escala con el modelo constrido para
+#gstools. Con un valor len_scal=50 este da un resultado nefasto. Creo que la
+#diferencia está en que los modelos se definen distintos en uno y el otro
+modelito = gs.Gaussian(dim=2,var=0.5,len_scale=5)
 
 #generamos unos puntos al tuntun para evaluar en ellos la funcion creada
 rng = np.random.default_rng(seed = 123456789)
@@ -92,24 +98,33 @@ for i in range(xpyp.shape[1]):
     200*gausianilla(xpyp[:,i],sigmar1,mu1,norm1) +\
     10*gausianilla(xpyp[:,i],sigmar2,mu2,norm2)
 #creanmos el krigenador ;)
-krig = gs.krige.Universal(modelito,[xpyp[0,:],xpyp[1,:]],Vp,'linear')
-Vhat,var =krig([x,y],mesh_type='structured')
-krig.plot() #le dejo pintar a el el campo estimado
-ax.plot_wireframe(xm,ym,Vhat.T,color='r')
+krig = pkr.uk.UniversalKriging(xpyp[0,:],xpyp[1,:],Vp,modelito)
+Vhat,var =krig.execute('grid',x,y)
 
+#sobre la grafica de antes, pinto el campo estimado
+ax.plot_wireframe(xm,ym,Vhat,color='r')
 ax.scatter(xpyp[0,:],xpyp[1,:],Vp,'or')
 
+#sacamos los resultados en plano que se ven muy bien del resultado de kriging
 plt.figure()
-plt.contour(xm,ym,Vhat.T,20)
+plt.contourf(xm,ym,Vhat,30)
+plt.title('campo vhat')
+plt.colorbar()
+plt.figure()
+
+#pintamos juntos, curvas de nivel de ambos y puntos empleados en la regresión
+plt.contour(xm,ym,Vhat,20)
 plt.contour(xm,ym,V)
 plt.scatter(xpyp[0,:],xpyp[1,:])
 
+#pintamos el resutado en plano para comporar con el estimado
 plt.figure()
 plt.contourf(xm,ym,var,30)
-plt.title('varianza')
+plt.title('Varianza')
 plt.colorbar()
+
 
 plt.figure() 
 plt.contourf(xm,ym,V,30)
 plt.title('campo V')
-plt.colorbar(                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           )()
+plt.colorbar()
